14c0efdf0e67e26d6258ff7bb6c66e1096132735
goldenhearten7 <66818449+taruntailor7@users.noreply.github.com>
2022-12-05 17:43:26 +0530

    UI Completed starting routing

---
+++ blog-app/src/pages/login/Login.jsx
@@ -0,0 +1,17 @@
+import "./login.css"
+
+export const Login = () => {
+  return (
+    <div className="login">
+        <spna className="loginTitle">Login</spna>
+        <form className="loginForm">
+            <label>Email</label>
+            <input type="email" className="loginInput" placeholder="Enter your email..."/>
+            <label>Password</label>
+            <input type="password" className="loginInput" placeholder="Enter your password..."/>
+            <button className="loginButton">Login</button>
+        </form>
+        <button className="loginRegisterButton">Register</button>
+    </div>
+  )
+}
---
+++ blog-app/src/pages/register/Register.jsx
@@ -0,0 +1,19 @@
+import "./register.css"
+
+export const Register = () => {
+  return (
+    <div className="register">
+        <spna className="registerTitle">Register</spna>
+        <form className="registerForm">
+            <label>Username</label>
+            <input type="text" className="registerInput" placeholder="Enter your username..."/>
+            <label>Email</label>
+            <input type="email" className="registerInput" placeholder="Enter your email..."/>
+            <label>Password</label>
+            <input type="password" className="registerInput" placeholder="Enter your password..."/>
+            <button className="registerButton">Register</button>
+        </form>
+        <button className="registerLoginButton">Login</button>
+    </div>
+  )
+}
