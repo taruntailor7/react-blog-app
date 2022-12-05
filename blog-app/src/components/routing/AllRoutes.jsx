029e0549f0ed8321d1afb0eac2953ade2946756b
goldenhearten7 <66818449+taruntailor7@users.noreply.github.com>
2022-12-05 18:25:52 +0530

    routing...

--- blog-app/src/components/routing/AllRoutes.jsx
+++ blog-app/src/components/routing/AllRoutes.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes } from "react-router-dom";
 import { Home } from '../../pages/home/Home';
 import { Login } from '../../pages/login/Login';
+import { NotFound } from '../../pages/notfound/NotFound';
 import { Register } from '../../pages/register/Register';
 import { Settings } from '../../pages/settings/Settings';
 import { Single } from '../../pages/single/Single';
@@ -10,12 +11,13 @@ import Write from '../../pages/write/Write';
 export const AllRoutes = () => {
   return (
     <Routes>
-         <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<Home />}></Route>
         <Route path="/register" element={<Register />}></Route>
         <Route path="/login" element={<Login />}></Route>
-        <Route path="/single" element={<Single />}></Route>
-        <Route path="/settings" element={<Settings />}></Route>
         <Route path="/write" element={<Write />}></Route>
+        <Route path="/settings" element={<Settings />}></Route>
+        <Route path="/post/:postId" element={<Single />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
     </Routes>
   );
 };
