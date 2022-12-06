cd7c7466c563547bc7860550fb5bc006f937be78
goldenhearten7 <66818449+taruntailor7@users.noreply.github.com>
2022-12-07 00:09:34 +0530

    categories and image uploaded api is completed

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ import mongoose from "mongoose"
 import authRoute from "./routes/auth.route.js"
 import usersRoute from  "./routes/users.route.js"
 import postRoute from "./routes/posts.route.js"
+import categoriesRoute from "./routes/categories.route.js"
+import multer from "multer"
 
 dotenv.config();
 const app = express();
@@ -25,9 +27,27 @@ const connection = async () =>{
     }
 }
 
+const storage = multer.diskStorage({
+    destination:(req,file,callbackFn) =>{
+        callbackFn(null,"images")
+    }, 
+    filename:(req,file,callbackFn)=>{
+        callbackFn(null,"xyz.png")
+    }
+})
+
+const upload =  multer({storage:storage});
+app.post("/upload", upload.single("file"),(req,res) =>{
+    res.send({
+        status: true,
+        message:"File has been uploaded"
+    });
+});
+
 app.use("/auth", authRoute);
 app.use("/users",usersRoute);
 app.use("/posts",postRoute);
+app.use("/categories",categoriesRoute);
 
 app.listen(3050, ()=>{
     try{
--- api/routes/categories.route.js
+++ api/routes/categories.route.js
@@ -1,6 +1,37 @@
 import express from 'express';
-import postModel from "../models/posts.model.js"
+import categoriesModel from "../models/categories.model.js"
 
 const router = express.Router();
+
+//Post Category
+router.post('/', async (req, res)=>{
+    const category = req.body
+    try {
+        let existCategoryWithName = await categoriesModel.findOne({name : category.name})
+        if(existCategoryWithName){
+            res.status(400).send({
+                message: "Category already exists"
+            });
+        } else {
+            let newCategory = await categoriesModel.create(category)
+            return res.send(newCategory);
+        }  
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
+
+//Get Category
+router.get('/', async (req, res)=>{
+    try {
+        let categories = await categoriesModel.find();
+        res.send({
+            status: true,
+            data: categories
+        })
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
 
 export default router
