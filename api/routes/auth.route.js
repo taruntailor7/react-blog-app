15b888992df9962b874572424f7bdcf83aad7d46
goldenhearten7 <66818449+taruntailor7@users.noreply.github.com>
2022-12-06 16:37:06 +0530

    password bcrypted feature added

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import userModel from "../models/users.model.js"
+import bcrypt from "bcryptjs";
 
 const router = express.Router();
 
@@ -10,18 +11,26 @@ router.post('/register',async (req, res) => {
         let { username, email, password} = user;
 
         let userExists = await userModel.findOne({email});
+        let usernameExists = await userModel.findOne({username});
         if(userExists){
             return res.status(400).send({
                 status: false,
                 message: 'User already exists'
             })
         }
+        else if(usernameExists){
+            return res.status(400).send({
+                status: false,
+                message: 'Username already taken'
+            })
+        }
         else{
+            password = bcrypt.hashSync(password);
             let newUser = await userModel.create({
                 username,email,password
             })
             newUser = newUser.toJSON();
-
+            delete newUser.password;
             return res.send(newUser)
         }
     } catch (error) {
